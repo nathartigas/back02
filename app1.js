83ed2fb73ccaa24700d51f8b592d81c83d7a03fd
nathartigas <nathartigasna@gmail.com>
2024-03-28 15:28:32 -0300

    impressao de arrays

---
+++ app1.js
@@ -0,0 +1,16 @@
+// console.log('nomes');
+
+// const { nomes } = require("./dados");
+
+// let pessoas = require('./dados');
+
+// console.log(pessoas);
+// console.log(pessoas[2]);
+
+// console.log(pessoas.nomes)
+
+let {nomes, frutas, Soma}=require('./dados');
+console.log(nomes);
+console.log(frutas);
+console.log(Soma(5,55));
+
---
+++ dados.js
@@ -0,0 +1,16 @@
+let nomes =['adimilson','benevides','clemilton'];
+
+let frutas=['Abacaxi','Banana','Cereja']
+//exposição explicita do array
+// module.exports=nomes;
+// module.exports=frutas;
+
+function Soma(a,b){
+    return a+b;
+}
+
+module.exports={
+    nomes,
+    frutas,
+    Soma
+}
